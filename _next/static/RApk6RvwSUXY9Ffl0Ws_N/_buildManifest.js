5f1b69890f98374425188b1691959edf9568d0f4
acaptutorials <acaptutorials@users.noreply.github.com>
2024-08-06 06:43:34 +0000

    deploy: 6a1cedd2ad0aafa9435099226cc0b9aec205061a

---
+++ _next/static/RApk6RvwSUXY9Ffl0Ws_N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/54fde772ad89017e.css","static/chunks/955-aac9d57ac5e0120b.js","static/chunks/pages/index-59bed55da94e6a71.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
